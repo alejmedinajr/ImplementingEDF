c59db9f53072074f82cd5cedd68b6d2e4699c908
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-05-03 12:41:13 -0500

    First commit
    includes graph.py, graphgenerator.py, and darpexperimentation. Also includes original readme and requirement files

---
+++ Code/DarpExperimentation.py
@@ -0,0 +1,109 @@
+import copy
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import csv
+import math
+from Graph import Graph
+from GraphGenerator import createRandomGraph
+
+def findRidesServed(graph, requestOrder, timeLimit):
+    """
+    This function will calculate the number of rides able to be served when following a particular schedule
+    :param requestOrder: A list of edges (pairs of vertices) in the order that they should be served
+    :param timeLimit: The time limit the server has to serve the ride requests
+    :return: The number of rides served in the given time limit, when serving in the given order
+    """
+    i = 0
+    t = 1
+    ridesServed = 1
+    if (timeLimit == 0):
+        ridesServed = 0
+    
+    # WHILE (time hasn't expired, and there are still edges left)
+    while (t < timeLimit and i < len(requestOrder) - 1):
+        # I am thinking I can check to make sure the deadline is respected before I do anything else, and this should be enough
+        if (t > graph.getDeadline(requestOrder[i][0]+1, requestOrder[i][1]+1)):
+            return -math.inf # give -infinty value when a deadline is violated
+
+        # IF (the end location of one request is the same as the next requests start location)
+        if (requestOrder[i][1] == requestOrder[i + 1][0]):
+            ridesServed += 1  # increment the number of rides served the same ammount as the time
+            t += 1
+        # ELIF (the server must travel to the start of the next request, but runs out of time to complete that request)
+        elif (t == timeLimit - 1):
+            t += 1
+        # ELSE (the server had to travel without serving a request)
+        else:
+            # travel to beginning of next request and serve it. Increment the time 1 unit more than the rides served
+            ridesServed += 1
+            t += 2
+        i += 1
+
+    return ridesServed
+
+
+def permuteRidesServed(graph, possibleRequests, i, timeLimit, ridesServed):
+    """
+    Recursively Add the rides served of each possible permutation of requests to the array.
+    :param possibleRequests: List of all edges (pairs of vertices/rides)
+    :param i: The index of the request to be switched in the ordering
+    :param timeLimit: Time limit for serving requests
+    :param ridesServed: Array of all possible rides served
+    :return: Array of all possible rides served given permutations
+    """
+    # IF (the last edge in the list has been reached
+    if i == len(possibleRequests):  # get out of recursion
+        # Add the rides served gotten from the particular permutation/order of requests
+        ridesServed.append(findRidesServed(graph, possibleRequests, timeLimit))
+    else:
+        for j in range(i, len(possibleRequests)):
+            # Switch the ordering of the list of edges
+            possibleRequests[i], possibleRequests[j] = possibleRequests[j], possibleRequests[i]
+
+            permuteRidesServed(graph, possibleRequests, i+1, timeLimit, ridesServed)
+
+            # switch the edge back to its original place in the order for next loop
+            possibleRequests[i], possibleRequests[j] = possibleRequests[j], possibleRequests[i]
+    return ridesServed
+
+
+def opt(graph, timeLimit):
+    """
+    Algorithm to find the Max possible number of rides served for a given graph and time limit
+    :param graph: Describes the Start and end point of each request to be served
+    :param timeLimit: Time allotted to serve requests
+    :return: Max possible number of rides served in the given amount of time
+    """
+    # list of every possible pair of vertices
+    possibleEdges = []
+    ridesServed = []
+
+    # create list of every possible pair of vertices
+    for i in range(graph.V):
+        for j in graph.graph[i]:
+            possibleEdges.append((i, j))
+
+    # Find the number of rides that can be served for each possible permutation/ordering of requests
+    ridesServed = permuteRidesServed(graph, possibleEdges, 0, timeLimit, ridesServed)
+
+    if (len(possibleEdges) == 0):
+        return 0
+    else:
+        return max(ridesServed)
+
+
+if __name__ == '__main__':    
+    
+    h=Graph(9,0)
+    h.addEdgeWithDeadline(1,2,5)
+    h.addEdgeWithDeadline(2,3,5)
+    h.addEdgeWithDeadline(3,4,4)
+    h.addEdgeWithDeadline(4,5,5)
+    h.addEdgeWithDeadline(1,6,0)
+    h.addEdgeWithDeadline(6,7,4)
+    h.addEdgeWithDeadline(7,8,0)
+    h.addEdgeWithDeadline(8,9,4)
+    print(h.edges)
+
+    print("opt",opt(h,5))
+    f=copy.deepcopy(h)
---
+++ Code/GraphGenerator.py
@@ -0,0 +1,107 @@
+import itertools
+import random
+import math
+
+from Graph import Graph
+
+def generateRandomDeadline(p, min, max):
+    isRequest = random.random() <= p # for generating random deadlines, we only want p percent of deadlines (p is parameterized)
+    if (isRequest): return random.randint(min, max) # want random deadline, random deadline value will be in range of (min, max)
+    else: return 0 # no random deadline, return 0 to signify this is not a request
+
+def createRandomGraphWithoutDeadlines(numberOfVertices, numberOfEdges, ID):
+    i = 0
+    g = Graph(numberOfVertices, ID)
+    while i < (numberOfEdges):
+        u = random.randint(1, numberOfVertices)
+        v = random.randint(1, numberOfVertices)
+        while u == v:
+            v = random.randint(1, numberOfVertices)
+
+        g.addEdgeWithDeadline(u, v, math.inf) # all edges will have infinite deadlines
+        i += 1
+
+    return g
+
+def createRandomGraphWithDeadlines(numberOfVertices, numberOfEdges, ID, p, min, max):
+    i = 0
+    g = Graph(numberOfVertices, ID)
+    while i < (numberOfEdges):
+        u = random.randint(1, numberOfVertices)
+        v = random.randint(1, numberOfVertices)
+        while u == v:
+            v = random.randint(1, numberOfVertices)
+
+        g.addEdgeWithDeadline(u, v, math.inf) # all edges will have infinite deadlines
+        i += 1
+
+    return g
+
+def generateRequestGraphsWithoutDeadlines(numberOfNodes):
+    '''This method will create every possible directed graph with the given number of nodes'''
+
+    locations = []
+    lengths = []
+    # first just get a list of numbers for the locations
+    for i in range(numberOfNodes):
+        locations.append(i + 1)
+    # find all the permutations of 2 of these locations to get all the possible edges
+    possibleRequests = list(itertools.permutations(locations, 2))
+    possibleEdgePermutations = []
+    # find all the permutations/combinations of all of these edges to get the possible graphs.
+    for i in range(2 * numberOfNodes):
+        possibleEdgePermutations.append(list(itertools.combinations(possibleRequests, i + 1)))
+        lengths.append(len(list(itertools.combinations(possibleRequests, i + 1))))
+    
+    possibleRequestGraphs = []
+    
+    c = 0
+    for i in range(numberOfNodes * 2):  # total number of edges
+        for j in range(lengths[i]):
+            graph = Graph(numberOfNodes, c)
+            for k in range(i + 1):
+                graph.addEdgeWithDeadline(possibleEdgePermutations[i][j][k][0], possibleEdgePermutations[i][j][k][1], math.inf)
+            possibleRequestGraphs.append(graph)
+            c += 1
+
+    return possibleRequestGraphs
+
+
+def generateRequestGraphsWithDeadlines(numberOfNodes, p, min, max, num):
+    '''This method will create every possible directed graph with the given number of nodes'''
+
+    locations = []
+    lengths = []
+    currentGraphsWithDeadlines = 0
+    # first just get a list of numbers for the locations
+    for i in range(numberOfNodes):
+        locations.append(i + 1)
+    # find all the permutations of 2 of these locations to get all the possible edges
+    possibleRequests = list(itertools.permutations(locations, 2))
+    possibleEdgePermutations = []
+    # find all the permutations/combinations of all of these edges to get the possible graphs.
+    for i in range(2 * numberOfNodes):
+        possibleEdgePermutations.append(list(itertools.combinations(possibleRequests, i + 1)))
+        lengths.append(len(list(itertools.combinations(possibleRequests, i + 1))))
+    
+    possibleRequestGraphs = []
+    
+    c = 0
+    for i in range(numberOfNodes * 2):  # total number of edges
+        for j in range(lengths[i]):
+            graph = Graph(numberOfNodes, c)
+            # we want the graph to have random deadlines
+            if(random.random() >= 0.5 and currentGraphsWithDeadlines <= num):
+                currentGraphsWithDeadlines +=1 # increase the number of graphs we have generated so far with deadlines
+                for k in range(i + 1): # create the graph with random deadlines
+                    graph.addEdgeWithDeadline(possibleEdgePermutations[i][j][k][0], possibleEdgePermutations[i][j][k][1], generateRandomDeadline(numberOfNodes, p, min, max))
+
+            # either we created all of the number of graphs we want with deadlines or this is not a graph meant to have deadlines        
+            else: 
+                for k in range(i + 1): # create the graph with infite deadliens
+                    graph.addEdgeWithDeadline(possibleEdgePermutations[i][j][k][0], possibleEdgePermutations[i][j][k][1], math.inf)
+                    
+            possibleRequestGraphs.append(graph) # add the newly created graph to our collection of graphs
+            c += 1 # increase the id
+
+    return possibleRequestGraphs
