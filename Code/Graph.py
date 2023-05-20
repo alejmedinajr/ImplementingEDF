532c6c4b23598dfb0732d80a51cf95a5479e8f38
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-05-20 16:35:06 -0500

    Update Graph.py
    The visual function now has enhanced readability and shows the two edge deadlines when applicable.

--- Code/Graph.py
+++ Code/Graph.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-
+from networkx.drawing.nx_pydot import graphviz_layout
 
 class Graph:
     def __init__(self, *args):
@@ -32,18 +32,19 @@ class Graph:
         :param saveTo: The file where the png graph will be saved to
         """
         G = nx.DiGraph() # create a nx directed graph for vizualization
+        
         for i in self.edges: # add each edge of the graph to the nx graph 
             # only add an edge if it has a deadline value that is not zero
             if (self.edges[i] != 0):
                 G.add_edge(i[0],i[1], deadline=self.edges[i])
 
         # computes the layout using the Fruchterman-Reingold algorithm
-        pos = nx.spring_layout(G)
+        pos = nx.spring_layout(G, k = 50, seed = 5) # k is the distance between the nodes, seed 5 is being used to avoid getting different graph visuals for the same graph instance, maybe this seed value could be parameterized
 
         # draw the graph
-        nx.draw(G, pos=pos, with_labels=True)
+        nx.draw(G, pos=pos, node_color='#00b4d9', with_labels=True, width=2, arrowsize=15)
         deadlines = nx.get_edge_attributes(G,'deadline') # save the deadlines in a collection
-        nx.draw_networkx_edge_labels(G,pos,edge_labels=deadlines) # include the deadlines on the graph
+        nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=deadlines, label_pos=0.25, font_size=7, alpha =0.8) # include the deadlines on the graph
         plt.savefig(saveTo) # save the vizualization to a png file
         plt.show() # show the graph
         
