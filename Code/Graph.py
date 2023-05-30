83b718751a237494b8d85f1143c41a33316b2068
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-05-30 16:49:00 -0500

    Graph visualization now works for edf and opt
    The visualization function now has bigger labels, and it supports different coloring for edf and opt solutions. This was done by redrawing the actual requests served (parameter returned by the actual algorithm) in order to make this easier. EDF is yello and OPT is red. There is also a new helper function that is meant to force the timelimit to be the minimum value of the maximum deadline and the given timelimit. This is to ensure that no unnecessary iterations occur if we have small deadlines and are given a large timelimit. The timelimit itself was also added to the graph visualization as a title. This means that the timelimit is now visible with every graph that is visually printed.

--- Code/Graph.py
+++ Code/Graph.py
@@ -25,29 +25,31 @@ class Graph:
         #    s += str(i) + ":" + str(self.graph[i]) + "\n"
         #return "graph id: " + str(self.id) + "\n" + str(self.edges)
     
-    def visualizeGraph(self, saveTo):
+    def visualizeGraph(self, timelimit , saveTo):
         """
         This function uses networkx and matplot to create an image vizualization of the directed graph
         :param saveTo: The file where the png graph will be saved to
         """
         G = nx.DiGraph() # create a nx directed graph for vizualization
         
-        for i in self.edges: # add each edge of the graph to the nx graph 
+        for edge in self.edges: # add each edge of the graph to the nx graph 
             # only add an edge if it has a deadline value that is not zero
-            if (self.edges[i][1] != 0):
-                G.add_edge(i[0],i[1], deadline=str(self.edges[i][0]) + "/-/" + str(self.edges[i][1]))
+            if (self.edges[edge][1] != 0 and self.edges[edge][1] != 1):
+                G.add_edge(edge[0],edge[1], deadline=str(self.edges[edge][0]) + "/-/" + str(self.edges[edge][1]))
 
         # computes the layout using the Fruchterman-Reingold algorithm
         pos = nx.spring_layout(G, k = 50, seed = 5) # k is the distance between the nodes, seed 5 is being used to avoid getting different graph visuals for the same graph instance, maybe this seed value could be parameterized
-
+        ax = plt.gca()
+        ax.set_title("Timelimit = " + str(timelimit))
         # draw the graph
         nx.draw(G, pos=pos, node_color='#00b4d9', with_labels=True, width=2, arrowsize=15)
         deadlines = nx.get_edge_attributes(G,'deadline') # save the deadlines in a collection
-        nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=deadlines, label_pos=0.25, font_size=7, alpha =0.8) # include the deadlines on the graph
-        plt.savefig(saveTo + ".png") # save the vizualization to a png file
+        nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=deadlines, label_pos=0.35, font_size=10, alpha =0.8) # include the deadlines on the graph
+        #plt.title('my random fig')
+        plt.savefig(saveTo + "Visual.png") # save the vizualization to a png file
         plt.show() # show the graph
 
-    def visualizeGraphSolution(self, timesServed, requestsServed, saveTo):
+    def visualizeGraphSolution(self, timelimit, timesServed, requestsServed, method, saveTo):
         """
         This function uses networkx and matplot to create an image vizualization of the directed graph
         :param saveTo: The file where the png graph will be saved to
@@ -56,7 +58,7 @@ class Graph:
         timesServed = list(filter(lambda a: a != 'x', timesServed))
         for edge in self.edges: # add each edge of the graph to the nx graph 
             # only add an edge if it has a deadline value that is not zero
-            if (self.edges[edge] != 0):
+            if (self.edges[edge][1] != 0 and self.edges[edge][1] != 1):
                 if edge in requestsServed:
                     # request was served
                     serviceTime = timesServed[requestsServed.index(edge)]
@@ -66,13 +68,20 @@ class Graph:
 
         # computes the layout using the Fruchterman-Reingold algorithm
         pos = nx.spring_layout(G, k = 50, seed = 5) # k is the distance between the nodes, seed 5 is being used to avoid getting different graph visuals for the same graph instance, maybe this seed value could be parameterized
-
+        ax = plt.gca()
+        ax.set_title("Timelimit = " + str(timelimit))
         # draw the graph
         nx.draw(G, pos=pos, node_color='#00b4d9', with_labels=True, width=2, arrowsize=15)
         deadlines = nx.get_edge_attributes(G,'deadline') # save the deadlines in a collection
-        nx.draw_networkx_edge_labels(G,pos=pos,edge_labels=deadlines, label_pos=0.25, font_size=7, alpha=0.8) # include the deadlines on the graph
+        colors = nx.get_edge_attributes(G,'color') # save the deadlines in a collection
+        if method == "OPT":
+            nx.draw_networkx_edges(G, pos, edgelist=requestsServed, arrowsize = 15, width=2, edge_color='red') # opt solution in red
+        else: 
+            nx.draw_networkx_edges(G, pos, edgelist=requestsServed, arrowsize = 15, width=2, edge_color='yellow') # edf solution in yellow
 
-        plt.savefig(saveTo) # save the vizualization to a png file
+        nx.draw_networkx_edge_labels(G,pos=pos, edge_labels=deadlines, label_pos=0.35, font_size=10, alpha=0.8) # include the deadlines on the graph
+        
+        plt.savefig(saveTo + "VisualSolution" + str(method) + ".png") # save the vizualization to a png file
         plt.show() # show the graph
 
 
@@ -137,6 +146,17 @@ class Graph:
 
     def getNumberOfRequests(self):
         return len(self.edges) # the length of the edges is the same as the number of requests. This means we can organize permuations based on this
+
+    def getTimeLimit(self, timelimit):
+        print(self.edges)
+        maxDeadline = -math.inf
+        for edge in self.edges:
+            print(edge)
+            print(self.edges[edge][1])
+            if self.edges[edge][1] > maxDeadline:
+                maxDeadline = self.edges[edge][1]
+
+        return min(maxDeadline, timelimit)
 
     def copy(self):
         return self.graph.copy()
