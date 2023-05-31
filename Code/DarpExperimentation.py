58b6c7b12e15a64c220a16cd1bffbc03f234e621
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-05-31 13:11:20 -0500

    Fixed OPT and EDF Timing
    The timing for OPT and EDF was messed up because I was thinking of the time in terms of intervals. I first needed to change the current time to start at two, since this is the first time any possible request can be served (i.e. time 1 is spent traveling to any location so time 2 is when it can actually be served). The second thing that needed to be fixed was the bounds. The bound needed to be changed to <= since a request could be served at time 5 if its deadline was 5. Although this changed the solutions slightly in terms of the graph visualization, the actual optimal solution did not seem to change too much. Another big issue I found with edf was that the inclusion of deadlines meant the way the requests were sorted was wrong. Initially, all requests only had a deadline, so the lambda sorting function sorted it based on that one value for each key. Since there are now two values per key, the sorting was being done on the release time. This was the reason why the edf results were now incorrect. Once I realized this, I changed the sorting to be based on the second element, which is the deadline. This seemed to fix the main issue with EDF. I am more confident of EDF now, as I have tested it with old archived test cases, as well as some of the easier to manually computer new interesting test cases with nonzero release times.

--- Code/DarpExperimentation.py
+++ Code/DarpExperimentation.py
@@ -17,19 +17,19 @@ def findRidesServed(graph, requestOrder, timeLimit, paths, timeRecord):
     :return: A set containing the number of rides served in the given time limit and the path taken, when serving in the given order
     """
     currentRequest = 0 # variable responsible for keeping track of the requests. Must not exceed size of requestOrder 
-    currentTime = 1 # variable responsible for keeping track of time. Must not exceed timeLimit
+    currentTime = 2 # variable responsible for keeping track of time. Must not exceed timeLimit. Starts at two because that is the first time we are able to serve requests
     ridesServed = 0 # variable responsible for keeping track of the number of requests served for this permutation of request orders
     pathTaken = []
     timeFootprint = []
     if (timeLimit == 0): return 0 # if the time limit is zero, then we can go ahead and return 0, since we know there are no requests that can be served with this time limit
     
     # We want to see how many requests can be served before the timeLimit is reached or before we serve all requests
-    while (currentTime < timeLimit and currentRequest < len(requestOrder)):
+    while (currentTime <= timeLimit and currentRequest < len(requestOrder)):
         # make sure the current deadline is respected
-        if (graph.getReleaseTime(requestOrder[currentRequest][0], requestOrder[currentRequest][1]) <= currentTime < graph.getDeadline(requestOrder[currentRequest][0], requestOrder[currentRequest][1])):
+        if (graph.getReleaseTime(requestOrder[currentRequest][0], requestOrder[currentRequest][1]) <= currentTime <= graph.getDeadline(requestOrder[currentRequest][0], requestOrder[currentRequest][1])):
             ridesServed += 1 # immediately serve the request
             pathTaken.append(requestOrder[currentRequest])
-            timeFootprint.append(currentTime + 1)
+            timeFootprint.append(currentTime)
         # if the next request is not continuous with the current request, then a jump needs to be made, so currentTime needs to be incremented to reflect this     
         if ((currentRequest < len(requestOrder) - 1) and (not requestOrder[currentRequest][1] == requestOrder[currentRequest+1][0])):
             currentTime += 1 # we have to make a jump since the next request is not connected
@@ -64,11 +64,11 @@ def opt(graph, timeLimit):
         #print(formatPathServed(ridesServed[ridesServed[0].index(max(ridesServed)[0])][1]))
     
         optimalSolution = max(ridesServed)
-        print(timeRecord[ridesServed.index(optimalSolution)])
-        print(paths[ridesServed.index(optimalSolution)])
+        #print(timeRecord[ridesServed.index(optimalSolution)])
+        #print(paths[ridesServed.index(optimalSolution)])
         return optimalSolution, timeRecord[ridesServed.index(optimalSolution)], paths[ridesServed.index(optimalSolution)] # return the max possible number of rides that can be served to give the optimal solution
 
-def updateRequests(currentTime, requests):
+def updateRequests(currentTime, requests, windowSize):
     """
     EDF helper function used to update the available requests that can be served by ensuring that requests from a collection are only added to availableRequests if the release time and deadlines are respected.
     :param currentTime: The variable used to represent the currentTime in the EDF Algorithm instance
@@ -81,7 +81,7 @@ def updateRequests(currentTime, requests):
         deadline = requests[r][1] # the second value is the deadline 
         #print(str(currentTime) + ":" + str(deadline))
             
-        if releaseTime <= currentTime < deadline: # a request must have a relase time that is at least the current time and a deadline that is no greater than the current time
+        if releaseTime <= currentTime <= deadline: # a request must have a relase time that is at least the current time and a deadline that is no greater than the current time
             availableRequests.append(r) # a new request can be added to the collection of available requests
     return availableRequests # return the collection of available requests
     
@@ -93,66 +93,71 @@ def edf(graph, timeLimit):
     :return: The maximum number of rides that can be served based on the EDF algorithm
     """
     windowSize = 2 # the amount of time units we can look forward
-    currentTime = 1 # current time starts at 1 because it is assumed the first time unit will be used to move to any initial location
+    currentTime = 2 # current time starts at 2 because it is assumed the first time unit will be used to move to any initial location, therefore 2 can be the earliest time a request can be served
     ridesServed = 0 # number of rides that have been served
-    requests = dict(sorted(graph.edges.items(), key=lambda x: x[1])) # all edges that are in a given graph sorted initially by earliest deadline since we want to serve earlier deadlines first if possible
+    requests = dict(sorted(graph.edges.items(), key=lambda x: x[1][1])) # all edges that are in a given graph sorted initially by earliest deadline since we want to serve earlier deadlines first if possible
     availableRequests = [] # available requests that we are able to serve, must be updated at the beginning of every iteration, and if a request is served
     requestsServed = [] # a collection to keep track of every request that was served and in what order it was served (mainly used for debugging purposes)
     timeServed = [] # a collection to keep track of what time the requests were actually served
     inactive = False
     while currentTime <= timeLimit: # the algorithm ends when the time limit is reached
-        availableRequests = updateRequests(currentTime, requests) # the available requests needs to be updated at the start of every iteration before we serve any rides
-
+        availableRequests = updateRequests(currentTime, requests, windowSize) # the available requests needs to be updated at the start of every iteration before we serve any rides
+        #print("WINDOWSIZE: " + str(windowSize))
+        #print(availableRequests)
         if len(availableRequests) > 0: # if there are requests we can serve, we should serve them
             ridesServed += 1 # serve the request 
             requestsServed.append(availableRequests[0]) # the first index of available requests is the one served since the request collection was sorted based on deadlines 
             currentRequest = availableRequests[0] # used to keep track of the current request before it is deleted 
             del availableRequests[0] # delete the request that was just served
             del requests[currentRequest] # delete the request from the request collection to make sure it is not possible served again
+            timeServed.append(currentTime)
             currentTime += 1 # increment the current time to reflect we have served a request
             windowSize = 2 # this will reset the windowSize if it was ever updated. It is important that after a request is served, the window size is set back to 2
-            if inactive: 
-                timeServed.append(currentTime-1) # subtract one from time current time when appeneding. If we were inactive, then we could have made the jump and waited until release time
-                inactive = False # reset boolean flag
-            else: timeServed.append(currentTime)
-            availableRequests = updateRequests(currentTime, requests) # serving a request means we must update the available requests, this is due to the current time changing (previously existing requests may be unservable)
+            availableRequests = updateRequests(currentTime, requests, windowSize) # serving a request means we must update the available requests, this is due to the current time changing (previously existing requests may be unservable)
+            #print(availableRequests)
             if len(availableRequests) > 0 and not currentRequest[1] == availableRequests[0][0]: # check if a jump needs to be made
+                #print(str(currentRequest[1]) + "->" + str(availableRequests[0][0]))
                 currentTime+=1 # a jump will be required, so we must increment the current time by one time unit to reflect this
+                #availableRequests = updateRequests(currentTime+1, requests, windowSize) # serving a request means we must update the available requests, this is due to the current time changing (previously existing requests may be unservable)
                 timeServed.append('x')
-
         else: # there are no available requests, we may have to increase the window size to be able to serve requests
             if windowSize < timeLimit: # only increase the window size if the current window size is smaller than the time limit
                 windowSize += 1 # increase the window size
                 currentTime += 1 # the action of increasing the window size still requires us to increase the current time by one unit since a time unit elapsed (we were just not able to serve anything)
                 timeServed.append('x')
-                inactive = True
-    
+                
     #print(timeServed)
     #print("REQUESTS SERVED: " + str(requestsServed)) # nice debugging way to see which requests were served before the final solution is returned            
     return ridesServed, timeServed, requestsServed # return the number of requests that were served by the EDF algorithm
+
 
 def runTestCases(testFolder):
     """
     This function provides a way to run test cases from a specific directory. The visual of each graph is saved as a png file and shown to the user.
     :param testFolder: Describes the root folder where the test cases are.
     """
+    #for file in glob.glob(testFolder + "\\test*Paper.txt"): # find every file in the specified folder that is a test file (test#.txt)
     for file in glob.glob(testFolder + "\\test*.txt"): # find every file in the specified folder that is a test file (test#.txt)
-    #for file in glob.glob(testFolder + "\\test1.txt"): # find every file in the specified folder that is a test file (test#.txt)
         print("Running: " + file)
         graphInfo = GraphGenerator.generateGraphFromFile(file)
         graph = graphInfo[0] # the graph is at index 0
         timeLimit = Graph.getTimeLimit(graph, graphInfo[1]) # the timeLimit is at index 1
+        #print(timeLimit)
         Graph.visualizeGraph(graph, timeLimit, file) # showing visual to the user, also saves as a png image to the folder
-        #optInfo = opt(graph, timeLimit)
-        #print("opt: " + str(optInfo[0])+ " with timeLimit: " + str(timeLimit))
-        #Graph.visualizeGraphSolution(graph, timeLimit, optInfo[1], optInfo[2], "OPT" , file)
+        optInfo = opt(graph, timeLimit)
+        print("opt: " + str(optInfo[0])+ " with timeLimit: " + str(timeLimit))
+        Graph.visualizeGraphSolution(graph, timeLimit, optInfo[1], optInfo[2], "OPT" , file)
         edfInfo = edf(graph, timeLimit)
+        print(timeLimit)
         print("edf: " + str(edfInfo[0])+ " with timeLimit: " + str(timeLimit))
         Graph.visualizeGraphSolution(graph, timeLimit, edfInfo[1], edfInfo[2], "EDF" , file)
 
 
 if __name__ == '__main__':    
     # To run the test cases, the lines until the next empty line need to be uncommented.
+    #testFolder = "TestCases"
+    testFolder = "TestCases\\Archived Test Cases"
+    runTestCases(testFolder)
     testFolder = "TestCases"
     runTestCases(testFolder)
     
