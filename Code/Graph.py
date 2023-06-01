1a93b9ab77e570b270a1b76b2f5ed3f565efe4dc
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-06-01 10:39:13 -0500

    commented out graph popup
    The visual popup for each graph needed to be manually closed, so I had to comment out the code that shows it. However, doing so results in the graphs being drawn over each other. This was a quick fix done by including plt.clf() at the end.

--- Code/Graph.py
+++ Code/Graph.py
@@ -48,6 +48,7 @@ class Graph:
         #plt.title('my random fig')
         plt.savefig(saveTo + "Visual.png") # save the vizualization to a png file
         #plt.show() # show the graph
+        plt.clf()
 
     def visualizeGraphSolution(self, timelimit, timesServed, requestsServed, method, saveTo):
         """
@@ -82,6 +83,7 @@ class Graph:
         nx.draw_networkx_edge_labels(G,pos=pos, edge_labels=deadlines, label_pos=0.35, font_size=10, alpha=0.8) # include the deadlines on the graph
         
         plt.savefig(saveTo + "VisualSolution" + str(method) + ".png") # save the vizualization to a png file
+        plt.clf()
         #plt.show() # show the graph
 
 
