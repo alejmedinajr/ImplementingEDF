802e1efb78392b9492a5d1b93dfce1a61f7411de
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-06-06 14:42:12 -0500

    Timing and fix to crashing problem when there are no solutions
    I added a way for the run to be timed. The time is saved to csv along with the test name and solution. There was also a problem when no solutions were found. Initially, zero was returned when no solution is found, but the solution visualizer function requires values from a list of the solution. This is because when rides were served, additional data was returned as well. This was changed to make the code compatible for the case where there are no rides served.

--- Code/EDF.py
+++ Code/EDF.py
@@ -1,6 +1,7 @@
 from Graph import Graph
+import time 
 
-def updateRequests(currentTime, requests, windowSize):
+def updateRequests(currentTime, requests, windowSize, timelimit):
     """
     EDF helper function used to update the available requests that can be served by ensuring that requests from a collection are only added to availableRequests if the release time and deadlines are respected.
     :param currentTime: The variable used to represent the currentTime in the EDF Algorithm instance
@@ -13,7 +14,7 @@ def updateRequests(currentTime, requests, windowSize):
         releaseTime = requests[r][0] # the first value is the release time
         deadline = requests[r][1] # the second value is the deadline 
         
-        if releaseTime <= currentTime + windowSize - 2 <= deadline: # we can look ahead using the window size if need be. Since the window size is default of 2, this also makes sure we do not look ahead if we do not need to (windowsize - 2 would be 0 in this case) 
+        if releaseTime <= currentTime + windowSize - 2 <= deadline and currentTime + windowSize - 2 < timelimit: # we can look ahead using the window size if need be. Since the window size is default of 2, this also makes sure we do not look ahead if we do not need to (windowsize - 2 would be 0 in this case) 
             availableRequests.append(r) # a new request can be added to the collection of available requests
     
     return availableRequests # return the collection of available requests
@@ -25,6 +26,8 @@ def edf(graph, timeLimit):
     :param timeLimit: The maximum amount of time units the algorithm is allowed to serve rides for
     :return: The maximum number of rides that can be served based on the EDF algorithm
     """
+    start = time.time()
+
     windowSize = 2 # the amount of time units we can look forward
     currentTime = 2 # current time starts at 2 because it is assumed the first time unit will be used to move to any initial location, therefore 2 can be the earliest time a request can be served
     ridesServed = 0 # number of rides that have been served
@@ -35,7 +38,7 @@ def edf(graph, timeLimit):
 
     while currentTime < timeLimit and len(requests) + len(availableRequests) > 0 and windowSize < timeLimit: # the algorithm ends when the time limit is reached
       
-        availableRequests = updateRequests(currentTime, requests, windowSize) # the available requests needs to be updated at the start of every iteration before we serve any rides
+        availableRequests = updateRequests(currentTime, requests, windowSize, timeLimit) # the available requests needs to be updated at the start of every iteration before we serve any rides
         
         if len(availableRequests) > 0: # if there are requests we can serve, we should serve them
             if windowSize > 2: # if the window size is larger than the default value (2), then that means the window increased
@@ -49,16 +52,14 @@ def edf(graph, timeLimit):
             del requests[currentRequest] # delete the request from the request collection to make sure it is not possible served again
             timeServed.append(currentTime)
             currentTime += 1 # increment the current time to reflect we have served a request
-            availableRequests = updateRequests(currentTime, requests, windowSize) # serving a request means we must update the available requests, this is due to the current time changing (previously existing requests may be unservable)
+            availableRequests = updateRequests(currentTime, requests, windowSize, timeLimit) # serving a request means we must update the available requests, this is due to the current time changing (previously existing requests may be unservable)
             if len(availableRequests) > 0 and not currentRequest[1] == availableRequests[0][0]: # check if a jump needs to be made
                 currentTime+=1 # a jump will be required, so we must increment the current time by one time unit to reflect this
-                timeServed.append('x')
+                
         else: # there are no available requests, we may have to increase the window size to be able to serve requests
             if windowSize < timeLimit: # only increase the window size if the current window size is smaller than the time limit
                 windowSize += 1 # increase the window size
-                timeServed.append('x')
-                
-    #print(timeServed)
-    #print("REQUESTS SERVED: " + str(requestsServed)) # nice debugging way to see which requests were served before the final solution is returned            
-    if ridesServed == 0: return 0
-    else: return ridesServed, timeServed, requestsServed # return the number of requests that were served by the EDF algorithm
+
+    end = time.time()
+    timeTaken = end - start    
+    return ridesServed, timeServed, requestsServed, timeTaken # return the number of requests that were served by the EDF algorithm
