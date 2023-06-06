d1e26a123c3bbd1c44c12b617279e3c8ab64e4bd
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-06-06 14:47:03 -0500

    CSV now prints all solutions and time
    The csv function now prints all of the results this way they can be looked at for trends in an easier fashion. Additionally, new batch tests were made for bigger tests but it seems for now the max requests can be 20 due to constraints of islice.

--- Code/DarpExperimentation.py
+++ Code/DarpExperimentation.py
@@ -6,7 +6,7 @@ from OPT import opt
 from EDF import edf
 import random
 
-def reportResults(optSolution, edfSolution, writer, file):
+def reportResults(optSolution, optTime, edfSolution, edfTime, writer, file):
     """
     This function provides a way to write flagged test cases to csv. The reported solutions for OPT and EDF are 
     compared and different flags can be thrown depending on both solutions in comparison to each other.
@@ -16,18 +16,21 @@ def reportResults(optSolution, edfSolution, writer, file):
     :param writer: The writer that is used to actually perform the function of writing to the specified csv file.
     :param file: The current test case file that was run, if there was something worthy of reporting in this test case, the file needs to be reported for easy user verification.
     """
-    if edfSolution == 2*optSolution: # need to report when edf solution is equal to 2*opt solution
-        writer.writerow(["|EDF| = 2*|OPT|", file, optSolution, edfSolution])
+    if edfSolution == optSolution: # record the solutions for every test case. If they are not equal, then a special flag will be written depending on the situation
+        writer.writerow(["|EDF| = |OPT|", file, optSolution, optTime, edfSolution, edfSolution])
     
-    if edfSolution != optSolution: # need to report when the two solutions are not the same
-        writer.writerow(["|EDF| differs from |OPT|", file, optSolution, edfSolution])
+    else: 
+        if edfSolution == 2*optSolution: # need to report when edf solution is equal to 2*opt solution
+            writer.writerow(["|EDF| = 2*|OPT|", file, optSolution, optTime, edfSolution, edfSolution])
 
-    if edfSolution > optSolution: # need to report when edf does better than opt (this should never happen)
-        writer.writerow(["|EDF| > |OPT|", file, optSolution, edfSolution])
+        elif edfSolution > optSolution: # need to report when edf does better than opt (this should never happen)
+            writer.writerow(["|EDF| > |OPT|", file, optSolution, optTime, edfSolution, edfSolution])
 
-    if 2*edfSolution < optSolution: # need to report when edf does has a solution more than twice as less than opt (should also never happen)
-        writer.writerow(["|EDF| < 2*|OPT|", file, optSolution, edfSolution])
+        elif 2*edfSolution < optSolution: # need to report when edf does has a solution more than twice as less than opt (should also never happen)
+            writer.writerow(["|EDF| < 2*|OPT|", file, optSolution, optTime, edfSolution, edfSolution])
 
+        else: # need to report when the two solutions are not the same
+            writer.writerow(["|EDF| differs from |OPT|", file, optSolution, optTime, edfSolution, edfSolution])
 
 def runTestCases(testFolder):
     """
@@ -41,10 +44,9 @@ def runTestCases(testFolder):
     
         for file in glob.glob(testFolder + "\\test*.txt"): # find every file in the specified folder that is a test file (test#.txt)
             print("Running: " + file) # print statement to let the user know which file test case is currently running
-            graphInfo = GG.generateGraphFromFile(file) # function to generate the graph from the specified file
+            graphInfo = gg.generateGraphFromFile(file) # function to generate the graph from the specified file
             graph = graphInfo[0] # the graph is at index 0
-            timeLimit = Graph.getTimeLimit(graph, graphInfo[1]) # the timeLimit is at index 1
-            #print(timeLimit)
+            timeLimit = graphInfo[1] # the timeLimit is at index 1
             Graph.visualizeGraph(graph, timeLimit, file) # save visual of the graph prior to any algorithms being run
             optInfo = opt(graph, timeLimit) # optInfo contains everything returned by opt algorithm
             print("opt: " + str(optInfo[0])+ " with timeLimit: " + str(timeLimit)) # display opt result to console
@@ -53,58 +55,71 @@ def runTestCases(testFolder):
             print("edf: " + str(edfInfo[0])+ " with timeLimit: " + str(timeLimit)) # display edf result to console
             Graph.visualizeGraphSolution(graph, timeLimit, edfInfo[1], edfInfo[2], "EDF" , file) # create visual of edf result and store it in test suite directory
             
-            reportResults(optInfo[0], edfInfo[0], writer, file)
+            reportResults(optInfo[0], optInfo[3], edfInfo[0], edfInfo[3], writer, file) # write any possible flags from results
 
 
 def runRandomTestCases(minNodes, maxNodes, minEdges, maxEdges, minTimelimit, maxTimelimit, f, p, min, max, graphsGenerated, testFolder, saveTo):
-    
+    """
+    This function provides a way to run test cases on randomly generated graphs. All results are saved to specified directory.
+    :param minNodes: The lower bound on the range of possible number of nodes for randomly generated graph
+    :param maxNodes: The upper bound on the range of possible number of nodes for randomly generated graph
+    :param minEdges: The lower bound on the range of possible number of edges for randomly generated graph
+    :param maxEdges: The upper bound on the range of possible number of edges for randomly generated graph
+    :param minTimelimit: The lower bound on the range of possible timelimit values
+    :param maxTimelimit: The upper bound on the range of possible timelimit values
+    :param f: The frequency of bidirectional edges. If f = 0, no bidirectional edges will be allowed, otherwise the value of f represents the frequency of bidirectional edges.
+    :param p: The percent of edges that will have deadlines (we only want a p percent of edges to be requests)
+    :param min: The minimum value that a deadline could be
+    :param max: The maximum value that a deadline could be
+    :param graphsGenerated: The total number of random graphs that will be generated
+    :param testFolder: The parent folder where the output files should be saved to
+    :param saveTo: The name the file should have as it is saved
+    """
     with open(testFolder + " - Report.csv", 'w', newline='') as report: # create a csv report of the results. This is where any flagged test cases will be reported
         writer = csv.writer(report) # create the writer for the csv file
-        fields = ["flag", "testcase", "OPT", "EDF"] # fields of what will be written to the csv
+        fields = ["flag", "testcase", "OPT", "OPT Time (s)", "EDF", "EDF Time (s)"] # fields of what will be written to the csv
         writer.writerow(fields) # writing the first row to csv file - contains all the fields
     
-        numberOfNodes = random.randint(minNodes, maxNodes)
-        numberOfEdges = random.randint(minEdges, maxEdges)
-        timelimit = random.randint(minTimelimit, maxTimelimit)
         id = 0 # the id that will be used for the file saved of the randomly created graph
         for i in range(graphsGenerated):
+            numberOfNodes = random.randint(minNodes, maxNodes) # number of nodes for random graph will be [minNodes, maxNodes]
+            numberOfEdges = random.randint(minEdges, maxEdges) # number of edges for random graph will be [minEdges, maxEdges]
+            timelimit = random.randint(minTimelimit, maxTimelimit) # the timelimit is also random [minTimelimit, maxTimelimit]
+            
             file = testFolder + "\\" + saveTo + "_" + str(id) 
             graph = gg.createRandomGraphWithDeadlines(numberOfNodes, numberOfEdges, id, f, p, min, max)
             Graph.visualizeGraph(graph, timelimit, file) # save visual of the graph prior to any algorithms being run
+
             optInfo = opt(graph, timelimit) # optInfo contains everything returned by opt algorithm
             print("opt: " + str(optInfo[0])+ " with timeLimit: " + str(timelimit)) # display opt result to console
-            Graph.visualizeGraphSolution(graph, timelimit, optInfo[1], optInfo[2], "OPT" , file) # create visual of opt result and store it in test suite directory
+            if optInfo[0] > 0:
+                Graph.visualizeGraphSolution(graph, timelimit, optInfo[1], optInfo[2], "OPT" , file) # create visual of opt result and store it in test suite directory
             edfInfo = edf(graph, timelimit) # edge info contains everything returned by edf algorithm
             print("edf: " + str(edfInfo[0])+ " with timeLimit: " + str(timelimit)) # display edf result to console
-            Graph.visualizeGraphSolution(graph, timelimit, edfInfo[1], edfInfo[2], "EDF" , file) # create visual of edf result and store it in test suite directory
+            if edfInfo[0] > 0:
+                Graph.visualizeGraphSolution(graph, timelimit, edfInfo[1], edfInfo[2], "EDF" , file) # create visual of edf result and store it in test suite directory
             
-            reportResults(optInfo[0], edfInfo[0], writer, file)
-            id += 1
+            reportResults(optInfo[0], optInfo[3], edfInfo[0], edfInfo[3], writer, file) # write any possible flags from results
+            id += 1 # the id of the graph needs to be increased by 1
 
 
 if __name__ == '__main__':    
-    # To run the test cases, the lines until the next empty line need to be uncommented.
-    #testFolder = "TestCases"
-    #testFolder = "TestCases\\Archived Test Cases"
-    #runTestCases(testFolder)
+    inputTestSuite = ["TestCases\\Archived Test Cases", "TestCases"] # collection of known existing test suits for grpah input files
+    #inputTestSuite = ["TestCases\\Archived Test Cases"] # collection of known existing test suits for grpah input files
     
-    #testFolder = "TestCases"
-    #runTestCases(testFolder)
+    randomTestSuite = ["TestCases\\Randomly Generated Test Cases\\batch1", "TestCases\\Randomly Generated Test Cases\\batch2", "TestCases\\Randomly Generated Test Cases\\batch3"] # collection of where the random generated batches should be saved
     
-    testFolder = "TestCases\\Randomly Generated Test Cases\\batch1"
-    runRandomTestCases(3, 15, 3, 15, 5, 50, 0.75, 0.75, 2, 30, 1000, testFolder, "randomGeneratedTestCasesBatch1")
-    # Running random graphs with opt to verify that it works
-    #for n in range(20):
-    #    g = GraphGenerator.createRandomGraphWithDeadlines(4, random.randint(1, 7), n, random.random(), 0.8, 1, 8)
-    #    timeLimit = random.randint(0,15)
-        #print("opt: " + str(opt(g, timeLimit)) + " with timeLimit: " + str(timeLimit))
-    #    Graph.visualizeGraph(g, "randomGenerator4VerticesTest" + str(n) + ".png")
-    #graphs = GraphGenerator.generateRequestGraphsWithDeadlines(4, 0.8, 1, 7, 5)
-    #for g in graphs:
-    #    Graph.visualizeGraph(g, "randomRequestGeneratorWith4Vertices5CopiesTest" + str(g.id) + ".png")
-        #g = GraphGenerator.createRandomGraphWithDeadlines(4, random.randint(1, 7), 0, .6, 0.8, 1, 5)
-        #timeLimit = random.randint(0,15)
-        #print("opt: " + str(edf(g, timeLimit)) + " with timeLimit: " + str(timeLimit))
-        #Graph.visualizeGraph(g, "randomRequestGeneratorWithInfiniteTest" + str(g.id) + ".png")
+    largerRandomTestSuite = ["TestCases\\Randomly Generated Test Cases\\batch4", "TestCases\\Randomly Generated Test Cases\\batch5", "TestCases\\Randomly Generated Test Cases\\batch6"] # collection of where the random generated batches should be saved
+    #randomTestSuite = ["TestCases\\Randomly Generated Test Cases\\batch1"]
+    i = 1 # the batch numbe rneeds to be incremented so the batches are distinct
+    #for testFolder in randomTestSuite:
+        #runRandomTestCases(14, 15, 3, 9, 10, 60, 0.8, 1, 5, 30, 500, testFolder, "randomGeneratedTestCasesBatch" + str(i)) # run the random tests on randomly generated graphs
+        #i +=1 # increase to signify the next batch
 
-        
+    #for testFolder in inputTestSuite: # run all input test suite files
+    #    runTestCases(testFolder)
+
+    i = 4
+    for testFolder in largerRandomTestSuite:
+        runRandomTestCases(60, 60, 20, 20, 15, 45, 0.8, 1, 5, 30, 100, testFolder, "randomGeneratedTestCasesBatch" + str(i)) # run the random tests on randomly generated graphs
+        i += 1 # increase to signify the next batch
