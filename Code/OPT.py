0634c03e9b4c61cf4996f9454509a43cdc509ad7
alejmedinajr <100097809+alejmedinajr@users.noreply.github.com>
2023-06-08 14:35:39 -0500

    X character was still being appended to timing collection
    I removed the code that filters out all of the 'x' characters that were initially used to represent a request not being served at that time (this was used for debugging purposes). I left one statement that still did this, so some solutions included that character, which should not be there.

--- Code/OPT.py
+++ Code/OPT.py
@@ -29,7 +29,7 @@ def findRidesServed(graph, requestOrder, timeLimit, currentOptimalRidesServed, p
         # if the next request is not continuous with the current request, then a jump needs to be made, so currentTime needs to be incremented to reflect this     
             if ((currentRequest < len(requestOrder) - 1) and (not requestOrder[currentRequest][1] == requestOrder[currentRequest+1][0])):
                 currentTime += 1 # we have to make a jump since the next request is not connected
-                if (currentTime < timeLimit): timeFootprint.append('x')
+            
             currentRequest += 1 # update to reflect that regardless of if we served the current request, we need to move to the next request.
             
         currentTime += 1 # at this point, either a request was served, or we could not serve the request. The currentTime needs to be incremented by 1 to reflect this
